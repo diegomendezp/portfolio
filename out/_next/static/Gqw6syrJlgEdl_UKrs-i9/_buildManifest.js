03e6ecb2303254a1d8a667c3ac83c5e610755f77
Diego Méndez <diegomen15@gmail.com>
2023-04-12 15:15:43 +0200

    Deploy gh-pages last version

---
+++ out/_next/static/Gqw6syrJlgEdl_UKrs-i9/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/css/69f81e06845cd7ae.css","static/chunks/pages/index-c0d7c2830528f3c5.js"],"/_error":["static/chunks/pages/_error-816335b8591e0d03.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
